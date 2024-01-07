abd44541ebef358f78989bf18d53c4ad7e1bbeeb
Mikkel Kruse Johnsen <mikkel@xmedicus.com>
2024-01-07 17:01:59 +0100

    Add GStreamer Sharp

---
+++ Source/Libs/GstSharp/Iterator.cs
@@ -0,0 +1,15 @@
+namespace Gst {
+
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Runtime.InteropServices;
+
+	public partial class Iterator
+	{
+	        public IEnumerator GetEnumerator()
+        	{
+	            return this.GetEnumerator();
+        	}
+	}
+}
